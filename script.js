6d236eb2d11dc20b05e8fedc55dab827786a4ecd
spachauri718 <spachauri718@gmail.com>
2023-12-08 01:21:28 +0530

    final

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let box = document.getElementById('box');
 const heightofscreen = window.innerHeight - box.clientHeight
 const widthofscreen = window.innerWidth - box.clientWidth
 function handleEvent(){
-    let topmargin = Math.floor(Math.random()*1000)
+    let topmargin = Math.floor(Math.random()*heightofscreen)
+    let leftmargin = Math.floor(Math.random()*widthofscreen);
     box.style.marginTop= `${topmargin}px`;
+    box.style.marginLeft= `${leftmargin}px`;
 }
 
 
 box.addEventListener('mouseover',handleEvent)
-box.addEventListener('click',handleEvent)
